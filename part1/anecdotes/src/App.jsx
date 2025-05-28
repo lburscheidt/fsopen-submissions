65d44ee287feff98fd267b9e22cc2d09066affef
lburscheidt <burscheidt@zohomail.eu>
2025-05-28 19:04:58 +0200

    Complete exercise 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,15 +13,28 @@ const App = () => {
 	];
 
 	const [selected, setSelected] = useState(0);
+	const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+	const votesCopy = [...votes];
 
 	const nextAnecdote = () => {
 		setSelected(Math.floor(Math.random() * (anecdotes.length - 1)));
 	};
 
+	const voteForAnecdote = () => {
+		votesCopy[selected] += 1;
+		setVotes(votesCopy);
+	};
+
 	return (
 		<div>
+			<h1>Anecdote of the day</h1>
 			{anecdotes[selected]}
 			<br />
+			<p>has {votes[selected]} votes</p>
+			<br />
+			<button type="button" onClick={voteForAnecdote}>
+				vote
+			</button>
 			<button type="button" onClick={nextAnecdote}>
 				next anecdote
 			</button>
