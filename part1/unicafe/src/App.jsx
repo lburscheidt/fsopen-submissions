4dadb92e741d35b2796285949cf2c6f1aa0c3c7f
lburscheidt <burscheidt@zohomail.eu>
2025-05-28 17:56:55 +0200

    Complete exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,16 +8,24 @@ const Button = (props) => {
 	);
 };
 
+const StatisticLine = (props) => {
+	return (
+		<p>
+			{props.text} {props.value}
+		</p>
+	);
+};
+
 const Statistics = (props) => {
 	if (props.all > 0) {
 		return (
 			<>
-				<p>good {props.good}</p>
-				<p>neutral {props.neutral}</p>
-				<p>bad {props.bad}</p>
-				<p>all {props.all}</p>
-				<p>average {props.average}</p>
-				<p>positive {props.positive} %</p>
+				<StatisticLine text="good" value={props.good} />
+				<StatisticLine text="neutral" value={props.neutral} />
+				<StatisticLine text="bad" value={props.bad} />
+				<StatisticLine text="all" value={props.all} />
+				<StatisticLine text="average" value={props.average} />
+				<StatisticLine text="positive" value={props.positive} />
 			</>
 		);
 	}
