b32bd33fc6d85e5eac08d61162d7e170945f18ad
lburscheidt <burscheidt@zohomail.eu>
2025-05-30 21:43:04 +0200

    Complete exercises 2.18 - 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,55 +1,41 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-
-const Filter = (props) => {
-  return (
-    <label>
-      find countries
-      <input value={ props.searchInput } onChange={ props.handleSearchInput } />
-    </label>
-  );
-};
+import "./index.css";
+import countryService from "./services/countries";
+import CountriesList from "./components/CountriesList";
+import Filter from "./components/Filter";
 
 const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [countryNames, setCountryNames] = useState([])
   const [searchInput, setSearchInput] = useState("");
+  const [countries, setCountries] = useState([]);
 
-  /*create list of country names */
   useEffect(() => {
-    console.log("effect");
-    axios
-      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then((response) => {
-        const notes = response.data;
-        setCountryNames(notes.map((note) => (note.name.common)))
-      });
+    countryService.getAll().then((countries) => {
+      setCountries(countries);
+    });
   }, []);
 
-  const handleSearchInput = (event) => {
+  const handleSearchChange = (event) => {
+    console.log(event.target.value);
     setSearchInput(event.target.value);
   };
 
-  /*  const countriesToShow = searchInput
-      /*? countryNames.filter((c) => c.toLowerCase().includes(searchInput))
-      : countryNames;*/
-  const countriesLength = countryNames.filter((c) => c.toLowerCase().includes(searchInput)).length
+  const filteredCountries =
+    searchInput !== ""
+      ? countries.filter((c) =>
+        c.name.common.toLowerCase().includes(searchInput.toLowerCase()),
+      )
+      : countries;
 
-  const countriesToShow =
-    searchInput !== "" && countriesLength <= 10 ?
-      countryNames.filter((c) => c.toLowerCase().includes(searchInput)).map((item, index) => index ? <><br key={ c } />{ item }</> : <Fragment key={ c }>{ item }</Fragment>) :
-      "Too many countries"
-
-  console.log(countriesToShow)
-
-
-  /*const singleNames = countriesNames.map((c) => { c.name.common })
-  
-  console.log(singleNames)*/
   return (
     <>
-      <Filter searchInput={ searchInput } handleSearchInput={ handleSearchInput } />
-      <div>{ countriesToShow }</div>
+      <Filter
+        handleSearchChange={ handleSearchChange }
+        searchInput={ searchInput }
+      />
+      <CountriesList
+        searchInput={ searchInput }
+        filteredCountries={ filteredCountries }
+      />
     </>
   );
 };
